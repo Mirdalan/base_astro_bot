fe9101660200832dde449cbb9cdae7b048494dc7
Mirdalan <michrzan@gmail.com>
2018-11-08 21:05:26 +0100

    refactored tests.K

--- tests/test_fleet.py
+++ tests/test_fleet.py
@@ -6,7 +6,7 @@ from base_astro_bot.database_manager import DatabaseManager
 from tests.sample_ships_data import TEST_SHIPS
 
 
-class MockDiscordUser:
+class MockUserObject:
     def __init__(self, name):
         self.id = "%s#123" % name
         self.username = name
@@ -18,32 +18,32 @@ class FleetManagerTests(unittest.TestCase):
         self.mgr = DatabaseManager(self.db_name, log_file=None)
 
     def test_creates_and_reads_members(self):
-        self.mgr.add_and_get_member(MockDiscordUser("jeden"))
-        self.mgr.add_and_get_member(MockDiscordUser("dwa"))
-        self.mgr.add_and_get_member(MockDiscordUser("trzy"))
+        self.mgr.add_and_get_member(MockUserObject("jeden"))
+        self.mgr.add_and_get_member(MockUserObject("dwa"))
+        self.mgr.add_and_get_member(MockUserObject("trzy"))
         members = self.mgr.get_all_members()
         self.assertEqual(len(members), 3)
 
     def test_cannot_create_duplicate_members(self):
-        self.mgr.add_and_get_member(MockDiscordUser("jeden"))
-        self.mgr.add_and_get_member(MockDiscordUser("dwa"))
-        self.mgr.add_and_get_member(MockDiscordUser("trzy"))
-        self.mgr.add_and_get_member(MockDiscordUser("jeden"))
-        self.mgr.add_and_get_member(MockDiscordUser("dwa"))
-        self.mgr.add_and_get_member(MockDiscordUser("trzy"))
+        self.mgr.add_and_get_member(MockUserObject("jeden"))
+        self.mgr.add_and_get_member(MockUserObject("dwa"))
+        self.mgr.add_and_get_member(MockUserObject("trzy"))
+        self.mgr.add_and_get_member(MockUserObject("jeden"))
+        self.mgr.add_and_get_member(MockUserObject("dwa"))
+        self.mgr.add_and_get_member(MockUserObject("trzy"))
         members = self.mgr.get_all_members()
         self.assertEqual(len(members), 3)
 
     def test_update_ships(self):
-        self.mgr.update_member_ships(TEST_SHIPS, MockDiscordUser("mird"))
+        self.mgr.update_member_ships(TEST_SHIPS, MockUserObject("mird"))
         ships = self.mgr.get_all_ships()
         self.assertEqual(len(TEST_SHIPS), len(ships))
 
     def test_update_overwrites_ships(self):
-        self.mgr.update_member_ships(TEST_SHIPS, MockDiscordUser("mird"))
+        self.mgr.update_member_ships(TEST_SHIPS, MockUserObject("mird"))
         ships = self.mgr.get_all_ships()
         self.assertEqual(len(TEST_SHIPS), len(ships))
-        self.mgr.update_member_ships(TEST_SHIPS[1:], MockDiscordUser("mird"))
+        self.mgr.update_member_ships(TEST_SHIPS[1:], MockUserObject("mird"))
         ships = self.mgr.get_all_ships()
         self.assertEqual(len(TEST_SHIPS[1:]), len(ships))
 

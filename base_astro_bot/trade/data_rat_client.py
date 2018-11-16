cb82fda5cef975daf01c8a848d6ce81c6c1bfaca
Mirdalan <michrzan@gmail.com>
2018-11-16 15:52:38 +0100

    add_transaction method

--- base_astro_bot/trade/data_rat_client.py
+++ base_astro_bot/trade/data_rat_client.py
@@ -27,16 +27,26 @@ class DataClient:
         """
         return requests.get(self.base_url + 'commodity_prices').json()
 
-    def update_price(self, commodity, price, location, transaction_type="buy"):
+    def update_price(self, commodity_id, price, location_id, transaction_type="buy"):
         payload = {
                     "price_type": transaction_type,
-                    "price_location": location,
+                    "price_location": location_id,
                     "price_date": date.today().strftime("%Y-%m-%d"),
-                    "price_commodity": commodity,
+                    "price_commodity": commodity_id,
                     "price_unit_price": price
                   }
         return requests.patch(self.base_url + 'commodity_prices', json=payload).json()
 
+    def add_transaction(self, commodity_id, price, units, location_id, transaction_type):
+        payload = {
+                    "transaction_type": transaction_type,
+                    "transaction_location": location_id,
+                    "transaction_commodity": commodity_id,
+                    "transaction_units": units,
+                    "transaction_unit_price": price
+                  }
+        return requests.post(self.base_url + 'commodity_transactions', json=payload).json()
+
 
 if __name__ == '__main__':
-    print(DataClient().get_locations()[0])
+    print(DataClient().get_locations())

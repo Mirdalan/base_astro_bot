5008234724b0ad166713842ed1df5baef8250949
Mirdalan <michrzan@gmail.com>
2018-11-26 13:11:26 +0100

    WIP: add route calculation to commodity class

--- base_astro_bot/trade/data_structure.py
+++ base_astro_bot/trade/data_structure.py
@@ -1,25 +1,102 @@
 
 class DataItem(dict):
-    def __init__(self, item, parents=None, commodities=None, locations=None):
+    def __init__(self, item, parents=None, locations=None, prices=None):
         super().__init__(item)
-        self._parents = parents
-        self._commodities = commodities
+        self._prices = self._get_prices(prices)
         self._locations = locations
+        self._parents = self._get_parents(parents)
+
+    @property
+    def id(self):
+        return self.get('id')
+
+    def _get_parents(self, all_parents):
+        return []
+
+    def _get_prices(self, all_prices):
+        return {}
 
 
 class Commodity(DataItem):
+
+    def _get_prices(self, all_prices):
+        prices = {'buy': [], 'sell':[]}
+        for price in all_prices.values():
+            if price.commodity_id == self.id:
+                prices[price.type].append(price)
+        return prices
 
     @property
     def name(self):
         return self.get('commodity_name')
 
+    @property
+    def buy_prices(self):
+        return self._prices['buy']
+
+    @property
+    def best_buy(self):
+        return min(price.value for price in self.buy_prices)
+
+    @property
+    def sell_prices(self):
+        return self._prices['sell']
+
+    @property
+    def best_sell(self):
+        return max(price.value for price in self.buy_prices)
+
+    @property
+    def best_deal(self):
+        return self.best_sell - self.best_buy
+
+    def iterate_all_routes(self):
+        for buy in self.buy_prices:
+            for sell in self.sell_prices:
+                yield buy, sell
+
+    def iterate_filtered_routes(self, start_location=None, end_location=None, avoid=()):
+
+        def location_matches_query(price, start_end_query):
+            return not start_end_query or start_end_query.lower() in price.location.full_string.lower()
+
+        def not_avoiding_location(price):
+            return all(avoid_location.lower() not in price.location.full_string.lower() for avoid_location in avoid)
+
+        def location_matches_conditions(price, query):
+            return location_matches_query(price, query) and not_avoiding_location(price)
+
+        for buy, sell in self.iterate_all_routes():   # type: CommodityPrice, CommodityPrice
+            if location_matches_conditions(buy, start_location) and location_matches_conditions(sell, end_location):
+                yield buy, sell
+
+    def get_routes_table(self, money=50000, cargo=576, start_location=None, end_location=None, avoid=()):
+        routes_data = []
+        for buy, sell in self.iterate_filtered_routes(start_location, end_location, avoid):
+            bought_units = cargo * 100
+            spent_money = bought_units * buy.value
+            if spent_money > money:
+                bought_units = int(money / buy.value)
+                spent_money = money
+
+            routes_data.append({
+                'commodity': self.name,
+                'invested money': spent_money,
+                'income': round((sell.value * bought_units) - spent_money, 2),
+                'bought units': round(bought_units, 2),
+                'buy price': buy.value,
+                'buy location': buy.location.full_string,
+                'sell price': sell.value,
+                'sell location': sell.location.full_string
+            })
+        return routes_data
+
 
 class CommodityPrice(DataItem):
 
     @property
-    def commodity(self):
-        commodity_id = self.get('price_commodity')
-        return self._commodities.get(commodity_id)
+    def commodity_id(self):
+        return self.get('price_commodity')
 
     @property
     def location(self):
@@ -44,24 +121,45 @@ class Container(DataItem):
 
 class Location(DataItem):
 
+    def _get_parent(self, all_parents):
+        item_parent_id = self.get('location_container')
+        return all_parents.get(item_parent_id)
+
+    def _get_parents(self, all_parents):
+        parents = []
+        parent = self._get_parent(all_parents)
+        while parent:
+            parents.append(parent)
+            parent_id = parent.get('container_parent')
+            parent = all_parents.get(parent_id)
+        return parents
+
     @property
     def name(self):
         return self.get('location_name')
 
     @property
     def parent(self):
-        item_parent_id = self.get('location_container')
-        return self._parents.get(item_parent_id)
+        return self._parents[0]
+
+    @property
+    def planet(self):
+        try:
+            return self._parents[-2]
+        except IndexError:
+            return None
+
+    @property
+    def system(self):
+        return self._parents[-1]
 
     @property
     def parents_string(self):
-        parents_names = []
-        parent = self.parent
-        while parent:
-            parents_names.append(parent.name)
-            parent_id = parent.get('container_parent')
-            parent = self._parents.get(parent_id)
-        return " , ".join(parents_names)
+        return " , ".join([parent.name for parent in self._parents])
+
+    @property
+    def full_string(self):
+        return "%s @ %s" % (self.name, self.parents_string)
 
 
 def choose_item_class(items):
@@ -81,23 +179,19 @@ class DataStructure(dict):
         item_class = choose_item_class(items_list)
         super().__init__({item['id']: item_class(item, **kwargs) for item in items_list})
 
-    @property
-    def id(self):
-        return self.get('id')
-
     def match_exact_name(self, query):
         for item in self.values():
             if query.lower() == item.name.lower():
                 return item
 
     def match_names(self, query):
-        return [item['id'] for item in self.values() if query.lower() in item.name.lower()]
+        return [item.id for item in self.values() if query.lower() in item.name.lower()]
 
     def exclude_names(self, query):
-        return [item['id'] for item in self.values() if query.lower() not in item.name.lower()]
+        return [item.id for item in self.values() if query.lower() not in item.name.lower()]
 
     def match_and_exclude_names(self, match_query, exclude_query):
-        return [item['id'] for item in self.values()
+        return [item.id for item in self.values()
                 if match_query.lower() in item.name.lower() and exclude_query.lower() not in item.name.lower()]
 
     def auto_match_names(self, match_query, exclude_query):
--- base_astro_bot/trade/prices.py
+++ base_astro_bot/trade/prices.py
@@ -1,7 +1,7 @@
-from ..utils import MyLogger
-from ..database import DatabaseManager
-from .data_structure import DataStructure
-from .data_rat_structure import DataRatPrices
+from base_astro_bot.utils import MyLogger
+from base_astro_bot.database import DatabaseManager
+from base_astro_bot.trade.data_structure import DataStructure
+from base_astro_bot.trade.data_rat_client import TradeClient
 
 
 class PricesStructure:
@@ -10,15 +10,12 @@ class PricesStructure:
         self._log_file = log_file
         self.logger = MyLogger(log_file_name=self._log_file, logger_name="Trade Assistant logger", prefix="[TRADE]")
         self.database = self._get_existing_or_new_database_manager(database_manager)
-        self.data_rat = DataRatPrices()
+        self.data_rat = TradeClient()
         self.celestial_bodies = None   # type: DataStructure
         self.locations = None   # type: DataStructure
         self.commodities = None   # type: DataStructure
         self.prices = None   # type: DataStructure
         self.update_database()
-
-    def update_rat(self):
-        self.data_rat.update(self.prices)
 
     def _get_existing_or_new_database_manager(self, database_manager):
         if database_manager:
@@ -27,11 +24,19 @@ class PricesStructure:
             return DatabaseManager(log_file=self._log_file)
 
     def _update_data_structure(self):
-        self.celestial_bodies = DataStructure(self.data_rat.containers)
-        self.locations = DataStructure(self.data_rat.locations, parents=self.celestial_bodies)
-        self.commodities = DataStructure(self.data_rat.commodities)
-        self.prices = DataStructure(self.data_rat.prices, commodities=self.commodities, locations=self.locations)
+        self.celestial_bodies = DataStructure(self.data_rat.get_containers())
+        self.locations = DataStructure(self.data_rat.get_locations(), parents=self.celestial_bodies)
+        self.prices = DataStructure(self.data_rat.get_prices(), locations=self.locations)
+        self.commodities = DataStructure(self.data_rat.get_commodities(), prices=self.prices)
 
     def update_database(self):
         self._update_data_structure()
         self.database.save_trade_data(self.locations, self.prices)
+
+
+if __name__ == '__main__':
+    from tabulate import tabulate
+    from base_astro_bot.trade.data_rat_client import save_to_temp_file
+    prices = PricesStructure()
+    widow = prices.commodities.match_exact_name("widow")
+    save_to_temp_file(tabulate(widow.get_routes_table(), headers='keys'))

2125f8bfa9412c0b39102c1eae40aad60f99cae2
Mirdalan <michrzan@gmail.com>
2018-12-03 14:33:39 +0100

    added loaners to fleet manager.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md") as f:
 
 setup(
     name='base_astro_bot',
-    version='1.1.6rc1',
+    version='1.1.6',
     description='Base bot class for Star Citizen players',
     long_description=long_description,
     long_description_content_type="text/markdown",
